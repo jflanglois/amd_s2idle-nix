f8e9c0af5ad7262a27e81e2c6d7f89f7607d43f2
Julien Langlois <yourstruly@julienlanglois.me>
2024-08-03 18:45:49 -0400

    Update 2024-08-03

--- flake.nix
+++ flake.nix
@@ -10,14 +10,16 @@
       version = "1.0";
       src = pkgs.fetchgit {
         url = "https://gitlab.freedesktop.org/drm/amd.git";
-        rev = "74ac2fcc3ce886fcbf9b417cc8a139d6d03eb67e";
-        hash = "sha256-wIjk4JdiI30P/Djxi++ow3I2/utm+WcUWkmmbM+h2BA=";
+        rev = "730b18d9f946a3fb2c7c4b10b19c7db1ef6f0927";
+        hash = "sha256-87dWGeWOSOGfoeHEkOJP12Jv2f6+93iyist4vq2mTAY=";
       };
       installPhase = ''install -Dm755 scripts/amd_s2idle.py $out/bin/amd_s2idle'';
       format = "other";
       dependencies = with pkgs.python3Packages; [
+        distro
         packaging
         pyudev
+        systemd
       ];
       preFixup = ''
         makeWrapperArgs+=(--prefix PATH : ${pkgs.lib.makeBinPath [
