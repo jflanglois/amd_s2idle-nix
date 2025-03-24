4262f26042497cd672de8cf487dbc72ce40fde0e
Julien Langlois <yourstruly@julienlanglois.me>
2025-03-24 01:53:23 -0400

    Update upstream

--- flake.nix
+++ flake.nix
@@ -9,12 +9,12 @@
       pname = "amd_s2idle";
       version = "1.0";
       src = pkgs.fetchgit {
-        url = "https://gitlab.freedesktop.org/drm/amd.git";
-        rev = "b5d9b83d756608423bf3d3cecf4ad24eb0951e2a";
-        hash = "sha256-NnTY7u7zE7lVX0QAkywgnjhbLDinmNWYKGDTRiN7B/A=";
+        url = "git://git.kernel.org/pub/scm/linux/kernel/git/superm1/amd-debug-tools.git";
+        rev = "71882dd21a9b037c6cb99de56db6655d54f72c18";
+        hash = "sha256-DjqoCZn0I2Teoa7k4ogBNPuW9cj4+8u2XZLQmIdKEtE=";
       };
       installPhase = ''
-        install -Dm755 scripts/amd_s2idle.py $out/bin/amd_s2idle
+        install -Dm755 amd_s2idle.py $out/bin/amd_s2idle
       '';
       format = "other";
       dependencies = with pkgs.python3Packages; [
