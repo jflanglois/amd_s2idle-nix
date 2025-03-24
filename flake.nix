8a221dda80e2c51e242db735159436c3ba49a138
Julien Langlois <yourstruly@julienlanglois.me>
2025-03-24 01:45:13 -0400

    Update inputs

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
   outputs = { self, nixpkgs }: {
     packages.x86_64-linux.default = let
